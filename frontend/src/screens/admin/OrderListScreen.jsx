54c9d0149884d9addaf21046093c5ee98a5c6cd3
MBI2022008 <mbi2022008@iiita.ac.in>
2023-07-30 22:51:05 +0530

    pending

--- frontend/src/screens/admin/OrderListScreen.jsx
+++ frontend/src/screens/admin/OrderListScreen.jsx
@@ -6,31 +6,60 @@ import { FaTimes } from 'react-icons/fa';
 import Message from '../../components/Message';
 import Loader from '../../components/Loader';
 import { useGetOrdersQuery } from '../../slices/orderApiSlice';
+import { Form,Row,Col} from 'react-bootstrap';
 
 
 
 const OrderListScreen = () => {
   const {data: orders, isLoading,error} = useGetOrdersQuery();
   const [pending,setPending]= useState([]);
+  const [delivered,setDelivered] = useState([]);
   useEffect(() => {
     if (!isLoading && !error && orders) {
       const pendingOrders = orders.filter((order) => !order.isDelivered);
+      const deliveredOrders = orders.filter((order)=> order.isDelivered);
+      pendingOrders.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+      setDelivered(deliveredOrders);
       setPending(pendingOrders);
     }
   }, [isLoading, error, orders]);
-  const [delivered,setDelivered] = useState([]);
-  useEffect(()=>{
-    if(!isLoading && !error && orders){
-      const deliveredOrders = orders.filter((order)=> order.isDelivered);
-      setDelivered(deliveredOrders);
-    }
-  },[isLoading,error,orders]);
+  const [status,setStatus] = useState(0);
   console.log(delivered);
   console.log(pending);
-  
+  const handleStatusChange = (e) => {
+    setStatus(Number(e.target.value));
+  };
+  let show = [];
+  if (status === 1) {
+    show = delivered;
+  } else if (status === 2) {
+    show = pending;
+  } else {
+    show = orders;
+  }
   return (
     <>
+      <Row>
+      <Col>
       <h1>Orders</h1>
+      </Col>
+      <Col xl={2} l = {2} md = {2}>
+      <Form.Control as = 'select'
+      className='back'
+                            value={status}
+                            onChange={handleStatusChange}>
+                              <option key = 'delivered' value = {1}>
+                                Delivered
+                              </option>
+                              <option key = 'pending' value = {2}>
+                                Pending
+                              </option>
+                              <option key = 'All' value = {0}>
+                                All
+                              </option>
+                            </Form.Control>
+      </Col>
+      </Row>
       {isLoading? (<Loader />):error?<Message variant='danger'>
         error
       </Message>:(<Table striped bordered hover responsive className='table-sm'>
@@ -45,7 +74,7 @@ const OrderListScreen = () => {
           </tr>
         </thead>
         <tbody>
-            {orders.map((order) => (
+            {show.map((order) => (
               <tr key={order._id}>
                 <td>{order._id}</td>
                 <td>{order.user && order.user.name}</td>
